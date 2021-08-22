12d64dbeb8dde07a0466ce841827d910f8a2da51
Aditya Gupte <aditya198601@gmail.com>
2021-08-22 20:57:32 +0530

    update

---
+++ src/Components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default function BasicButtons() {
+  return (
+    
+      <Button color="secondary" variant="contained">Text</Button>
+      
+  );
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-
+import BasicButtons from '../Components/Button';
+import Card2 from '../Components/Card2';
+import Sl1 from '../Components/slider';
 
 
 export default function Home() {
@@ -8,6 +10,10 @@ export default function Home() {
     <React.Fragment>
         <Container fixed>
             <p> This is a test case</p>
+            <Sl1 />
+            <BasicButtons />
+            <Card2 />
+            
             
         </Container>
     </React.Fragment>
