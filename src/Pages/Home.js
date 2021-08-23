271cc942fc3df1e067aefbfb6999e6c490cd081a
Aditya Gupte <aditya198601@gmail.com>
2021-08-23 09:56:14 +0530

    test

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,13 @@ import Container from '@material-ui/core/Container';
 import BasicButtons from '../Components/Button';
 import Card2 from '../Components/Card2';
 import Sl1 from '../Components/slider';
-
+import BoxComponent from '../Components/Box1';
+import QuiltedImageList from '../Components/Imagelist1';
+import BasicButtonGroup from '../Components/Buttongroup1';
+import ColorCheckboxes from '../Components/Checkbox1';
+import RadioButtonsGroup from '../Components/Radio1';
+import BasicSelect from '../Components/Select1';
+import BasicSwitches from '../Components/Switch1';
 
 export default function Home() {
   return (
@@ -13,7 +19,12 @@ export default function Home() {
             <Sl1 />
             <BasicButtons />
             <Card2 />
-            
+            <BoxComponent />
+            <BasicButtonGroup />
+            <ColorCheckboxes />
+            <RadioButtonsGroup />
+            <BasicSelect />
+            <BasicSwitches />
             
         </Container>
     </React.Fragment>
